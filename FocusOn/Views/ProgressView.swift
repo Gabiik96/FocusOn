f2d19670a314f3247d4167bce1b7014357b75649
Gabriel Balta <g.balta96@gmail.com>
2020-06-04 20:56:55 +0100

    init commit

---
+++ FocusOn/Views/ProgressView.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressView.swift
+//  FocusOn
+//
+//  Created by Gabriel Balta on 04/06/2020.
+//  Copyright © 2020 Gabriel Balta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressView()
+    }
+}
---
+++ FocusOn/Views/TodayView.swift
@@ -0,0 +1,21 @@
+//
+//  TodayView.swift
+//  FocusOn
+//
+//  Created by Gabriel Balta on 04/06/2020.
+//  Copyright © 2020 Gabriel Balta. All rights reserved.
+//
+
+import SwiftUI
+
+struct TodayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TodayView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodayView()
+    }
+}
