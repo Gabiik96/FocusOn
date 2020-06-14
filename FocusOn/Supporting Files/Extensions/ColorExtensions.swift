af3778d32692beccaa45a85301ba74cb0b41e29c
Gabriel Balta <g.balta96@gmail.com>
2020-06-14 16:55:28 +0100

    Extensions files for Colors made

---
+++ FocusOn/Supporting Files/Extensions/ColorExtensions.swift
@@ -0,0 +1,27 @@
+//
+//  ColorExtensions.swift
+//  FocusOn
+//
+//  Created by Gabriel Balta on 10/06/2020.
+//  Copyright © 2020 Gabriel Balta. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension UIColor {
+    static let backgColor = UIColor(named: "backgroundColor")
+    static let tabColor = UIColor(named: "tabColor")
+    static let textColor = UIColor(named: "textColor")
+    static let iconColor = UIColor(named: "iconColor")
+    static let headerColor = UIColor(named: "headerColor")
+}
+
+extension Color {
+    static let backgColor = Color("backgroundColor")
+    static let tabColor = Color("tabColor")
+    static let textColor = Color("textColor")
+    static let iconColor = Color("iconColor")
+    static let headerColor = Color("headerColor")
+}
