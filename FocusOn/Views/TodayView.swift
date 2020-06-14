bd97f0bb8007169a791a5a50a8c8190c48faa4ff
Gabriel Balta <g.balta96@gmail.com>
2020-06-14 16:56:35 +0100

    CoreData implementation in TodayView

--- FocusOn/Views/TodayView.swift
+++ FocusOn/Views/TodayView.swift
@@ -7,14 +7,63 @@
 //
 
 import SwiftUI
-
+import CoreData
 struct TodayView: View {
+    
+    @Environment(\.managedObjectContext) var moc: NSManagedObjectContext
+    @FetchRequest(entity: Goal.entity(), sortDescriptors: []) var goals: FetchedResults<Goal>
+    
+    @State private var newGoal = ""
+    @State private var task1 = ""
+    @State private var task2 = ""
+    @State private var task3 = ""
+    
     var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("Goal for the day to focus on:")){
+                    HStack {
+                        TextField("Set your goal..", text: self.$newGoal)
+                        Button(action: {
+                            let goal = Goal(context: self.moc)
+                            goal.title = self.newGoal
+                            goal.createdAt = Date()
+                            
+                            do {
+                                try self.moc.save()
+                            } catch {
+                                print(error)
+                            }
+                            })
+                        
+                        {
+                            Image(systemName: "plus.circle.fill")
+                                .foregroundColor(.green)
+                                .imageScale(.large)
+                        }
+                    }
+                }.font(.headline)
+                
+                Section(header: Text("3 tasks to achieve your goal")){
+                    VStack{
+                        HStack {
+                            TextField("1️⃣ Define task", text: self.$task1)
+                        }
+                        HStack {
+                            TextField("2️⃣ Define task", text: self.$task2)
+                        }
+                        HStack {
+                            TextField("3️⃣ Define task", text: self.$task3)
+                        }
+                    }
+                }
+            }
+                        
+            .navigationBarTitle(Text("FocusOn Today"), displayMode: .inline)
+            
+       
         
-        VStack(alignment: .center) {
-            Text("FocusOn Today")
-                .bold()
-            Divider()
+
         }
     }
 }
