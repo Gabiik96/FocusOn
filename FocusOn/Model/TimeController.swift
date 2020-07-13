57519718c22bb274e368334fd6b698930274422b
Gabriel Balta <g.balta96@gmail.com>
2020-07-13 22:06:33 +0100

    TimeController refactor

--- FocusOn/Model/TimeController.swift
+++ FocusOn/Model/TimeController.swift
@@ -8,7 +8,9 @@
 
 import Foundation
 
-class TimeController {
+struct TimeController {
+    
+    let calendar = Calendar.current
     
     // Computed properties for specific period of time
     var today: Date {
@@ -19,22 +21,51 @@ class TimeController {
         return getDay(date: Calendar.current.date(byAdding: .day, value: -1, to: today)!)
     }
     
+    var lastWeek: Date {
+        return getDay(date: Calendar.current.date(byAdding: .day, value: -7, to: today)!)
+    }
+    
+    var lastMonth: Date {
+        return getDay(date: Calendar.current.date(byAdding: .day, value: -30, to: today)!)
+    }
+    
+    var twoMonthsAgo: Date {
+        return getDay(date: Calendar.current.date(byAdding: .day, value: -60, to: today)!)
+    }
+    
+    func startOfWeek(date: Date) -> Date {
+
+        return self.calendar.date(from: calendar.dateComponents([.weekOfYear, .yearForWeekOfYear], from: Date()))!
+    }
+    
+    func startOfMonth(for date: Date) -> Date {
+        
+        let components = calendar.dateComponents([.year, .month], from: date)
+        let startOfMonth = calendar.date(from: components)!
+
+        return startOfMonth
+    }
     
     // Functions to formate
     func getDay(date: Date) -> Date {
-        var calendar = Calendar.current
-        calendar.timeZone = TimeZone.current
-        return calendar.startOfDay(for: date)
+        return self.calendar.startOfDay(for: date)
     }
     
     
-    func formatteDateToString(date: Date) -> String {
+    func formattedDayToString(date: Date) -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.timeZone = TimeZone.current
         dateFormatter.dateFormat = "MMM d yyyy"
         let stringDate = dateFormatter.string(from: date)
         
         return stringDate
+    }
+    
+    func formattedMonthToString(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "LLLL yyyy"
+        let nameOfMonth = dateFormatter.string(from: date)
+        
+        return nameOfMonth
     }
     
 }
